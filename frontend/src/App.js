33501d709043af2309a057adfba4dd6b68d4f4c1
alokvr <alokmessi@gmail.com>
2026-03-05 17:17:54 +0530

    add: addded routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './screens/Home';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./screens/Home";
+import Productdetails from "./components/Productdetails";
+import Cart from "./components/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <main  className='py-3'>
-        <Container>
-            <Home/>
-        </Container>
-      </main>
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:id" element={<Productdetails />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap';
-import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect> 
             <Container>
-                <Navbar.Brand href='/'>MernShop</Navbar.Brand>
+                <Navbar.Brand>
+                <Link to={`/`} style={{textDecoration: "none"}}>MernShop</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls='nav-toggle'/>
                     <Navbar.Collapse id='nav-toggle'/>
                     <Nav>
-                        <Nav.Link><FaShoppingCart/> Cart</Nav.Link>
+                        <Nav.Link><FaShoppingCart/>
+                        <Link style={{textDecoration: "none"}} to={`/cart`}>
+                         Cart
+                         </Link>
+                         </Nav.Link>
                         <Nav.Link><FaUser/> Signin</Nav.Link>
                     </Nav>
             </Container>
